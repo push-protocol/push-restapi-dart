8568e10b8258db7e0580e5a82d6f3a0e779a65f1
akp111 <pradhanashish95@gmail.com>
2023-07-22 14:12:25 +0530

    minor-fix

--- lib/src/video/src/helper/get_ice_server_config.dart
+++ lib/src/video/src/helper/get_ice_server_config.dart
@@ -1,7 +1,7 @@
 import 'package:push_restapi_dart/push_restapi_dart.dart';
 
-Future<dynamic> getIceServerConfig() {
+Future<dynamic> getIceServerConfig() async{
   const path = '/v1/turnserver/iceconfig';
-  var data = http.get(path: path);
+  var data = await http.get(path: path);
   return data;
 }
