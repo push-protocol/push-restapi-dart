c8d2763f3696d711c236fdf5a4a00ea87852ede3
Mohammed S <shoaibmohammed92@gmail.com>
2023-08-02 17:15:31 +0530

    fix: bug fix

--- example/lib/functions/chat/fetch_requests.dart
+++ example/lib/functions/chat/fetch_requests.dart
@@ -3,7 +3,7 @@ import 'package:push_restapi_dart/push_restapi_dart.dart';
 void testFetchRequests() async {
   final result = await requests(
       toDecrypt: false,
-      accountAddress: 'eip155:0x2E06acc49D2B0724a3681B6b0C264a74786C98d5');
+      accountAddress: 'eip155:0x35B84d6848D16415177c64D64504663b998A6ab4');
 
   print(result);
   if (result != null && result.isNotEmpty) {
