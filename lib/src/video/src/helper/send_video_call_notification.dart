72de60e8d1eba0dac0b29b497dcdc42a82ebfff5
Madhur Gupta <madhur@push.org>
2023-08-25 14:48:37 +0530

    feat: Add partial implementation of request

---
+++ lib/src/video/src/helper/send_video_call_notification.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:push_restapi_dart/push_restapi_dart.dart';
+
+Future<void> sendVideoCallNotification(
+  UserInfoType userInfo,
+  VideoCallInfoType videoCallInfo,
+) async {
+  try {
+    final VideoDataType videoData = VideoDataType(
+      recipientAddress: videoCallInfo.recipientAddress,
+      senderAddress: videoCallInfo.senderAddress,
+      chatId: videoCallInfo.chatId,
+      signalData: videoCallInfo.signalData,
+      status: videoCallInfo.status,
+      callDetails: videoCallInfo.callDetails,
+    );
+
+    print('sendVideoCallNotification videoData: $videoData');
+
+    final senderAddressInCaip =
+        getCAIPWithChainId(videoCallInfo.senderAddress, userInfo.chainId);
+    final recipientAddressInCaip = getCAIPWithChainId(
+        videoCallInfo.recipientAddress, userInfo.chainId);
+
+    final notificationText = 'Video Call from ${videoCallInfo.senderAddress}';
+    final notificationType = NOTIFICATION_TYPE.TARGETTED;
+
+    await sendNotification(SendNotificationInputOptions(
+      senderType: 1,
+      signer: userInfo.signer,
+      pgpPrivateKey: userInfo.pgpPrivateKey,
+      chatId: videoCallInfo.chatId,
+      type: notificationType,
+      identityType: IDENTITY_TYPE.DIRECT_PAYLOAD,
+      notification: NotificationOptions(
+        title: notificationText,
+        body: notificationText,
+      ),
+      payload: PayloadOptions(
+        title: 'VideoCall',
+        body: 'VideoCall',
+        cta: '',
+        img: '',
+        additionalMeta: AdditionalMeta(
+          type: '${videoCallInfo.callType}+1',
+          data: jsonEncode(videoData),
+        ),
+      ),
+      recipients: recipientAddressInCaip,
+      channel: senderAddressInCaip
+  ));
+  } catch (err) {
+    print('Error occurred while sending notification for video call: $err');
+  }
+}
--- lib/src/video/src/video.dart
+++ lib/src/video/src/video.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../push_restapi_dart.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 typedef SetDataFunction = VideoCallData Function(VideoCallData);
 
-final VideoCallData initVideoCallData = VideoCallData();
+late VideoCallData initVideoCallData = VideoCallData();
 
 class VideoCallStateNotifier extends ChangeNotifier {
+late VideoCallData data;
+  late Signer signer;
+  late int chainId;
+  late String pgpPrivateKey;
+  late int callType;
+  late Function(MediaStream receivedStream, String senderAddress, bool? audio)
+      onReceiveStream;
+
+  // {
+  //   address: {
+  //     peerConnection: RTCPeerConnection,
+  //     iceCandidates: RTCIceCandidate[]
+  //   }
+  // }
+  late Map<String, Map<String, dynamic>> _rtcPeer = {};
+
   VideoCallStateNotifier() {
     videoCallData = VideoCallData();
   }
@@ -22,14 +38,38 @@ class VideoCallStateNotifier extends ChangeNotifier {
     log('setData: $videoCallData');
   }
 
+  // Video({
+  //   required this.signer,
+  //   required this.chainId,
+  //   required this.pgpPrivateKey,
+  //   required this.callType,
+  //   required setData,
+  //   required data,
+  //   required this.onReceiveStream,
+  // }) {
+  //   onReceiveStream = onReceiveStream;
+
+  //   // Initialize the react state
+  //   final container = ProviderContainer();
+  //   container.read(videoCallStateProvider);
+
+  //   // Initialize the class variable
+  //   data = initVideoCallData;
+
+  //   // Set the state updating function
+  //   setData = (SetDataFunction fn) {
+  //     container.read(videoCallStateProvider.notifier).setData(fn);
+  //   };
+  // }
+
   Future<void> create(VideoCreateInputOptions? options) async {
     bool audio = options?.audio ?? true;
     bool video = options?.video ?? true;
-    rtc.MediaStream? stream = options?.stream;
+    MediaStream? stream = options?.stream;
 
     try {
-      final rtc.MediaStream localStream = stream ??
-          await rtc.navigator.mediaDevices.getUserMedia({
+      final MediaStream localStream = stream ??
+          await navigator.mediaDevices.getUserMedia({
             // for frontend
             'video': video,
             'audio': audio,
@@ -53,45 +93,79 @@ class VideoCallStateNotifier extends ChangeNotifier {
     }
   }
 
-  Future<void> request(VideoRequestInputOptions options) async {}
+  Future<void> request({
+    required String senderAddress,
+    required List<String> recipientAddresses,
+    required String chatId,
+    Object? details,
+  }) async {
+    print(
+        "senderAddress $senderAddress \n recipientAddresses $recipientAddresses \n chatId $chatId details $details");
+
+    for (final recipientAddress in recipientAddresses) {
+      try {
+        // TODO: Set video call data with status INITIALIZED
+
+        // TODO: Get ice server config from get_ice_server_config
+        RTCPeerConnection peer = await createPeerConnection({
+          'iceServers':
+              // replace the below array with decrypted turn server API result
+              [
+            {
+              'urls': [
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302'
+              ]
+            }
+          ]
+        });
+
+        _rtcPeer[recipientAddress] = { 'peerConnection': peer, 'iceCandidates': [] };
+
+        // add local stream's tracks to RTC connection
+        // this.data.local.stream!.getTracks().forEach((track) {
+        //   peer.addTrack(track, this.data.local.stream!);
+        // });
+
+        // listen for remotePeer mediaTrack event
+        peer.onTrack = (event) {
+          // save the incoming stream (event.streams[0]) in the state
+        };
+
+        // listen for local iceCandidate and add it to the list of IceCandidates
+        peer.onIceCandidate =
+            (RTCIceCandidate candidate) => _rtcPeer[recipientAddress]?['iceCandidates'].add(candidate);
+
+        // create SDP Offer
+        RTCSessionDescription offer = await peer.createOffer();
+        // set SDP offer as localDescription for peerConnection
+        await peer.setLocalDescription(offer);
+
+        // send a notification containing SDP offer
+        // sendVideoCallNotification(
+        //   // TODO: fill this object
+        //   {
+        //       signer: ,
+        //       chainId: ,
+        //       pgpPrivateKey: ,
+        //     }, {
+        //       senderAddress,
+        //       recipientAddress,
+        //       status: VideoCallStatus.INITIALIZED,
+        //       chatId,
+        //       signalData: offer.toMap(),
+        //       env: //,
+        //       callType: //,
+        //       callDetails: details,
+        //     })
+      } catch (err) {
+        print('error in request $err');
+      }
+    }
+  }
 }
 
 final videoCallStateProvider =
     ChangeNotifierProvider<VideoCallStateNotifier>((ref) {
   return VideoCallStateNotifier();
 });
-
-class Video {
-  late VideoCallData data;
-  final Signer signer;
-  final int chainId;
-  final String pgpPrivateKey;
-  final int callType;
-  late Function(MediaStream receivedStream, String senderAddress, bool? audio)
-      onReceiveStream;
-  Map<String, dynamic> peerInstances = {};
-
-  Video({
-    required this.signer,
-    required this.chainId,
-    required this.pgpPrivateKey,
-    required this.callType,
-    required setData,
-    required data,
-    required this.onReceiveStream,
-  }) {
-    onReceiveStream = onReceiveStream;
-
-    // Initialize the react state
-    final container = ProviderContainer();
-    container.read(videoCallStateProvider);
-
-    // Initialize the class variable
-    data = initVideoCallData;
-
-    // Set the state updating function
-    setData = (SetDataFunction fn) {
-      container.read(videoCallStateProvider.notifier).setData(fn);
-    };
-  }
-}
