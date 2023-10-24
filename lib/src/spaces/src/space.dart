404e8ade7ced53d436f1490624a52aa7b919afa7
Madhur Gupta <madhur@push.org>
2023-10-24 15:54:33 +0530

    Add raiseHand method in Space class

--- lib/src/spaces/src/space.dart
+++ lib/src/spaces/src/space.dart
@@ -157,6 +157,39 @@ class SpaceStateNotifier extends ChangeNotifier {
     return result;
   }
 
+  Future<void> raiseHand() async {
+    final localAddress = getCachedWallet()!.address!;
+
+    // check is the caller is an active listener & modify listener data accordingly
+    bool isActiveListener = false;
+    for (int i = 0; i < data.liveSpaceData.listeners.length; i++) {
+      ListenerPeer listener = data.liveSpaceData.listeners[i];
+      if (listener.address == pCAIP10ToWallet(localAddress)) {
+        if (listener.handRaised == true) {
+          // listener's hand is already raised
+          return;
+        }
+
+        listener.handRaised = true;
+        isActiveListener = true;
+      }
+    }
+
+    if (!isActiveListener) {
+      throw Exception("Local user is not a listener");
+    }
+
+    // send a user activity message
+    sendLiveSpaceData(
+        messageType: MessageType.USER_ACTIVITY,
+        updatedLiveSpaceData: data.liveSpaceData,
+        content: CHAT.UA_LISTENER_REQUEST_MIC,
+        affectedAddresses: [localAddress],
+        spaceId: data.spaceId);
+
+    notifyListeners();
+  }
+
   _updateLocalUserRoom(Room? localRoom) {
     _room = localRoom;
     notifyListeners();
