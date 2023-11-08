7d3bbfca5ede90d61dde8e12e2abfefbc6b91602
Gbogboade Ayomide <ay4allcrown@gmail.com>
2023-11-08 18:54:39 +0100

    chore: combine your_spaces files

--- example/lib/views/spaces/__spaces.dart
+++ example/lib/views/spaces/__spaces.dart
@@ -6,8 +6,8 @@ export 'live_space/live_space_provider.dart';
 export 'live_space/space_request_view.dart';
 export 'live_space/tiles.dart';
 
-export 'hosted_by_you/your_spaces_provider.dart';
-export 'hosted_by_you/hosted_by_you_tab.dart';
+export 'your_spaces/your_spaces_provider.dart';
+export 'your_spaces/hosted_by_you_tab.dart';
 
 export 'space_requests/space_request_provider.dart';
 export 'space_requests/space_invites_screen.dart';
@@ -16,4 +16,4 @@ export 'space_tile.dart';
 export 'popular/popular_space_provider.dart';
 export 'popular/popular_tab.dart';
 
-export 'for_you/for_you_tab.dart';
+export 'your_spaces/for_you_tab.dart';
