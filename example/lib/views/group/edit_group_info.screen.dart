cd459e690e954f908fc720f05278022f38422fd9
gbogboadePush <135420608+gbogboadePush@users.noreply.github.com>
2023-12-22 12:35:23 +0100

    fix: Update groupImage in updateGroupProfile to allow null value & hide edit group info button (#86)
    * fix: Update groupImage in updateGroupProfile to allow null value & hide edit group info button
    
    * chore: remove white spaces
    
    ---------
    
    Co-authored-by: Gbogboade Ayomide <ay4allcrown@gmail.com>
    Co-authored-by: Madhur Gupta <madhur@push.org>

--- example/lib/views/group/edit_group_info.screen.dart
+++ example/lib/views/group/edit_group_info.screen.dart
@@ -110,7 +110,7 @@ class _EditGroupInfoScreenState extends ConsumerState<EditGroupInfoScreen> {
         chatId: groupInfo!.chatId,
         groupName: nameController.text.trim(),
         groupImage:
-            selectedFile != null ? composeImage() : groupInfo!.groupImage!,
+            selectedFile != null ? composeImage() : groupInfo!.groupImage,
         groupDescription: descriptionController.text.trim(),
       );
 
